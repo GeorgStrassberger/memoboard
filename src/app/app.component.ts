79e3f87e2db6e7ddf1030199f578dc65108f0522
Georg <georg.strassberger@googlemail.com>
2022-11-13 14:30:37 +0100

    TypeScript testig

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 
 import { Component } from '@angular/core';
-import { Firestore, collectionData, collection, setDoc, doc, deleteDoc, deleteField, updateDoc } from '@angular/fire/firestore';
+import { Firestore, collectionData, collection, setDoc, doc, deleteDoc, deleteField, updateDoc, CollectionReference, DocumentData, FieldPath, Query } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 import { Imemo } from './imemo';
 
@@ -16,20 +16,22 @@ export class AppComponent {
   inputMemo: string= '';
   
 
-
   constructor(public firestore: Firestore) {
     const collect: any = collection(firestore, 'memos');
+    
     this.memos$ = collectionData(collect, {idField: 'id'});
-    this.memos$.subscribe(this.logFirestore);   
+    this.memos$.subscribe(this.logFirestore);
   }
 
   //Log Firestore chances currently 
   logFirestore(memos: Imemo[]){
     console.log('FirestoreMemos: ',memos);
+    console.log('memos$: ',this.memos$)
   }
 
   addNote(){
-    const coll: any = collection(this.firestore, 'memos');// collection con firestore holen / Sammlung
+    const coll: CollectionReference = collection(this.firestore, 'memos');// collection con firestore holen / Sammlung
+
     if (this.inputMemo.trim() !== '') {
       setDoc(doc(coll),{text: this.inputMemo, person: 'Georg'});  //hinzufügen setDoc(param1, param2)
       //param1 = welches dokument in der Sammlung
@@ -39,7 +41,6 @@ export class AppComponent {
   }
 
   // sortierfunktion für die Memos bauen
-
 
 
 }
--- src/app/note/note.component.ts
+++ src/app/note/note.component.ts
@@ -1,6 +1,6 @@
 import { Component,Input, OnInit, Output } from '@angular/core';
 import { Imemo } from '../imemo';
-import { Firestore, collectionData, collection, setDoc, doc, deleteDoc, deleteField, updateDoc } from '@angular/fire/firestore';
+import { Firestore, collectionData, collection, setDoc, doc, deleteDoc, deleteField, updateDoc, CollectionReference, DocumentData } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-note',
@@ -14,18 +14,19 @@ export class NoteComponent implements OnInit {
 @Input() memo!: Imemo;
 
 
+// firestore über sub constructor einbinden 
+// ist das richig oder gibt es einen besseren weg intern auf die appComponent zuzugreifen?
 constructor(public firestore: Firestore) {
 
   }
 
   ngOnInit(): void {
-    
-    console.log('ID: ', this.memo.id);
+
     console.log('ID: ', this.memo.id);
   }
 
   async entfNote(memoId : string){
-    const collec: any = collection(this.firestore, 'memos');// collection con firestore holen / Sammlung
+    const collec: CollectionReference = collection(this.firestore, 'memos');// collection con firestore holen / Sammlung
     console.log('memoId: ',memoId);
 
     await deleteDoc(doc(collec, memoId));
